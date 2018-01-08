b9c298ff23114cc7114db82252b9b235f33cdedc
Kristof Komlossy <yiro4618@webis24.medien.uni-weimar.de>
2018-01-08 11:44:21 +0100

    +delete project option, fixed some inter-project bugs

--- mturk/mturk_manager/views/view.py
+++ mturk/mturk_manager/views/view.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render, redirect
+from mturk_manager.models import *
 import json
+import urllib.parse
+from django.contrib import messages
 
 def view(request, name):
     context = {}
+    name_quoted = name
+    name_project = urllib.parse.unquote(name_quoted)
+    db_obj_project = m_Project.objects.get(name=name_project)
 
-    list_ids = json.loads(request.GET['list_ids'])
-    print(list_ids)
+
+    try:
+        list_ids = json.loads(request.GET['list_ids'])
+    except (KeyError, json.JSONDecodeError):
+        messages.error(request, 'Please provide valid assignments')
+        return redirect('mturk_manager:project', name=name_quoted, permanent=True)
+
+
+    # print(list_ids)
     # context['queryset_account_mturk'] = m_Account_Mturk.objects.all()
-
+    for assignment in m_Assignment.objects.filter(
+        fk_hit__fk_batch__fk_project=db_obj_project, 
+        id__in=list_ids
+    ).select_related('fk_hit__fk_batch__fk_template__fk_template_assignment'):
+        print(assignment.fk_hit.id_hit)
+        print(assignment.fk_hit.fk_batch.fk_template.fk_template_assignment.template)
     # if request.method == 'POST':
     #     print(request.COOKIES)
     #     print(request.POST)
