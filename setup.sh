8a7e52f6eeb5b117e905eb49a636cb3abe8789fd
Kristof Komlossy <yiro4618@webis24.medien.uni-weimar.de>
2018-01-12 15:46:36 +0100

    *setup.sh

--- setup.sh
+++ setup.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 # do necessary inital tasks
 
-if ! python3 -c "import venv" &> /dev/null; then
+if ! dpkg -l "python3-venv" &> /dev/null; then
+# if ! python3 -c "import venv" &> /dev/null; then
     echo 'Please install python3-venv'
     exit
 fi
 
-git submodule init
-git submodule update --remote
+# git submodule init
+# git submodule update --remote
 
-mkdir venv
-python3 -m venv venv
+# mkdir venv
+# python3 -m venv venv
 
-source venv/bin/activate
+# source venv/bin/activate
 
-pip install django==2.0
-pip install whoosh==2.7.4
-pip install xmltodict
-pip install boto3
+# pip install django==2.0
+# pip install whoosh==2.7.4
+# pip install xmltodict
+# pip install boto3
 
-cd ./mturk
-python3 manage.py createcachetable
-python3 manage.py makemigrations viewer mturk_manager
-python3 manage.py migrate
+# cd ./mturk
+# python3 manage.py createcachetable
+# python3 manage.py makemigrations viewer mturk_manager
+# python3 manage.py migrate
