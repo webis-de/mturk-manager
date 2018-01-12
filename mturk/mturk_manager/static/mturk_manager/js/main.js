c88242bf9e171dc58bd3e4f3894a495b565bfd7b
Kristof Komlossy <yiro4618@webis24.medien.uni-weimar.de>
2018-01-12 14:06:35 +0100

    *improved settings page

--- mturk/mturk_manager/static/mturk_manager/js/main.js
+++ mturk/mturk_manager/static/mturk_manager/js/main.js
@@ -61,4 +61,19 @@ $(document).ready(function()
 	    const name_file = $(this)[0].files[0].name;
 	    $(this).parent().find('span').text(name_file);
 	})
+
+	$('#modal_update_account').on('show.bs.modal', function (event) 
+	{
+		const button = $(event.relatedTarget) // Button that triggered the modal
+		const id = button.data('id') // Extract info from data-* attributes
+		const name = button.data('name') // Extract info from data-* attributes
+		const key_secret = button.data('key_secret') // Extract info from data-* attributes
+		const key_access = button.data('key_access') // Extract info from data-* attributes
+		const modal = $(this)
+
+		modal.find('input[name="id"]').val(id);
+		modal.find('input[name="name"]').val(name);
+		modal.find('input[name="key_secret"]').val(key_secret);
+		modal.find('input[name="key_access"]').val(key_access);
+	})
 });
--- mturk/mturk_manager/views/settings.py
+++ mturk/mturk_manager/views/settings.py
@@ -1,41 +1,68 @@
 from django.shortcuts import render, redirect
 from mturk_manager.models import *
 import urllib.parse
+from django.contrib import messages
 
 def settings(request):
     context = {}
 
     if request.method == 'POST':
-        print(request.POST)
-        verified_input = verify_input(request)
-        if not verified_input == True:
-            context['success'] = False
-            context['queryset_mturk'] = m_Account_Mturk.objects.all()
-            return render(request, 'mturk_manager/settings.html', context)
-
-        create_account(request)
-
-        context['success'] = True
+        if request.POST['task'] == 'add_account':
+            add_account(request)
+        if request.POST['task'] == 'update_account':
+            update_account(request)
         return redirect('mturk_manager:settings', permanent=True)
 
     context['queryset_mturk'] = m_Account_Mturk.objects.all()
 
     return render(request, 'mturk_manager/settings.html', context)
 
-def create_account(request):
+
+def update_account(request):
+    if not verify_input_add_account(request):
+        return 
+
+    m_Account_Mturk.objects.filter(
+        id=request.POST['id']
+    ).update(
+        name = request.POST['name'],
+        key_access = request.POST['key_access'],
+        key_secret = request.POST['key_secret'],
+    )   
+
+    messages.success(request, 'Updated account successfully')
+
+def add_account(request):
+    if not verify_input_add_account(request):
+        return 
+
     m_Account_Mturk.objects.get_or_create(
         name = request.POST['name'],
         key_access = request.POST['key_access'],
         key_secret = request.POST['key_secret'],
     )
 
+    messages.success(request, 'Created account successfully')
 
-def verify_input(request):
-    if request.POST['name'].strip() == '':
-        return False
-    if request.POST['key_access'].strip() == '':
-        return False
-    if request.POST['key_secret'].strip() == '':
-        return False
+def verify_input_add_account(request):
+    valid = True
+    list_messages = []
 
-    return True
+    try:
+        if request.POST['name'].strip() == '':
+            valid = False
+            list_messages.append('Invalid name')
+        if request.POST['key_access'].strip() == '':
+            valid = False
+            list_messages.append('Invalid access key')
+        if request.POST['key_secret'].strip() == '':
+            valid = False
+            list_messages.append('Invalid secret key')
+    except KeyError:
+        list_messages.append('Unexpected error, please cry')
+        valid = False
+
+    for message in list_messages:
+        messages.error(request, message)
+
+    return valid
