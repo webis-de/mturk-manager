c6a5ee892fd299326f62a0f85aa6c8d3b9f47cb2
Kristof Komlossy <yiro4618@webis24.medien.uni-weimar.de>
2018-01-17 16:51:06 +0100

    *improved annotation page

---
+++ mturk/mturk_manager/static/mturk_manager/js/view.js
@@ -0,0 +1,180 @@
+$(document).ready(function()
+{
+	const glob_dict_assignemnts = {};
+
+	$(document).on('click', '.approve_assignment, .reject_assignment', function(e) {
+		const elem_button = $(this);
+		const id_assignment = elem_button.data('id_assignment');
+
+		if(elem_button.hasClass('active'))
+		{
+			delete glob_dict_assignemnts[id_assignment];
+			elem_button.removeClass('active');
+		} else {
+			if(elem_button.hasClass('approve_assignment'))
+			{
+				glob_dict_assignemnts[id_assignment] = {
+					state: 'approve',
+					message: undefined
+				};
+				const elem_button_reject = $('.reject_assignment[data-id_assignment="'+id_assignment+'"]');
+				elem_button_reject.removeClass('active');
+			} else {
+				glob_dict_assignemnts[id_assignment] = {
+					state: 'reject',
+					message: undefined
+				};
+				const elem_button_approve = $('.approve_assignment[data-id_assignment="'+id_assignment+'"]');
+				elem_button_approve.removeClass('active');
+			}
+
+			elem_button.addClass('active');
+		}
+
+		print_dict_assignment();
+	});
+
+	$(document).on('click', '[data-task="submit_annotations"]', function(e) {
+		const data = {};
+		data.task = 'submit_annotations';
+		data.dict_assignments = glob_dict_assignemnts;
+		data.message_reject_default = $('#input_message_reject_default').val();
+
+		if(data.message_reject_default.trim() == '')
+		{
+			let need_message_reject_default = false;
+			$.each(data.dict_assignments, function(i, e) {
+				if(e.message == undefined && e.state == 'reject')
+				{
+					need_message_reject_default = true;
+					return false;
+				}
+			});
+
+			if(need_message_reject_default)
+			{
+				$('#input_message_reject_default').parent().addClass('was-validated');
+				return
+			}
+		}
+
+		$.ajax({
+	        url: '',
+	        method: 'POST',
+	        contentType: 'application/json',
+	        headers: {'X-CSRFToken':$('input[name="csrfmiddlewaretoken"]').val()},
+	        data: JSON.stringify(data),
+	        success: function(result) {
+	        	console.log("done")
+	        	location.reload();
+	        }
+	    })
+	});
+
+	$(document).on('click', '[data-task="button_mturk_reject_selected"]', function(e) {
+		const elem_button = $(this);
+
+		$('input[name="checkbox_assignment"]:checked').each(function(index) {
+			const elem_checkbox = $(this);
+			const id_assignment = elem_checkbox.data('id_assignment');
+
+			const elem_button_approve = $('.approve_assignment[data-id_assignment="'+id_assignment+'"]');
+			elem_button_approve.removeClass('active');
+			
+			const elem_button_reject = $('.reject_assignment[data-id_assignment="'+id_assignment+'"]');
+			elem_button_reject.addClass('active');
+
+			glob_dict_assignemnts[id_assignment] = {
+				state: 'reject',
+				message: undefined
+			};
+
+			elem_checkbox.prop('checked', false);
+		})
+
+		print_dict_assignment();
+	});
+
+	$(document).on('click', '[data-task="button_mturk_approve_selected"]', function(e) {
+		const elem_button = $(this);
+
+		$('input[name="checkbox_assignment"]:checked').each(function(index) {
+			const elem_checkbox = $(this);
+			const id_assignment = elem_checkbox.data('id_assignment');
+
+			const elem_button_approve = $('.approve_assignment[data-id_assignment="'+id_assignment+'"]');
+			elem_button_approve.addClass('active');
+			
+			const elem_button_reject = $('.reject_assignment[data-id_assignment="'+id_assignment+'"]');
+			elem_button_reject.removeClass('active');
+
+			glob_dict_assignemnts[id_assignment] = {
+				state: 'approve',
+				message: undefined
+			};
+
+			elem_checkbox.prop('checked', false);
+		})
+
+		print_dict_assignment();
+	});
+
+	$(document).on('click', '.dropdown_reject_assignment_selected a', function(e) {
+		const elem_link = $(this);
+
+		$('input[name="checkbox_assignment"]:checked').each(function(index) {
+			const elem_checkbox = $(this);
+			const id_assignment = elem_checkbox.data('id_assignment');
+
+			const elem_button_approve = $('.approve_assignment[data-id_assignment="'+id_assignment+'"]');
+			elem_button_approve.removeClass('active');
+			
+			const elem_button_reject = $('.reject_assignment[data-id_assignment="'+id_assignment+'"]');
+			elem_button_reject.addClass('active');
+
+			glob_dict_assignemnts[id_assignment] = {
+				state: 'reject',
+				message: elem_link.text()
+			};
+
+			elem_checkbox.prop('checked', false);
+		});
+
+		print_dict_assignment();
+	});
+
+	$(document).on('click', '.dropdown_reject_assignment a', function(e) {
+		const elem_link = $(this);
+		const id_assignment = elem_link.data('id_assignment');
+
+		glob_dict_assignemnts[id_assignment] = {
+			state: 'reject',
+			message: elem_link.text()
+		};
+		
+		const elem_button = $('.reject_assignment[data-id_assignment="'+id_assignment+'"]');
+		elem_button.addClass('active');
+
+		const elem_button_approve = $('.approve_assignment[data-id_assignment="'+id_assignment+'"]');
+		elem_button_approve.removeClass('active');
+		
+		print_dict_assignment();
+	});
+
+	$(document).on('click', '.dropdown-menu a', function(e) {
+		e.preventDefault();
+	});
+
+	$(document).on('click', 'button', function(e) {
+		$(this).blur();
+	});
+	
+	$(document).on('click', '#dropdown_message_reject_default a', function(e) {
+		$('#input_message_reject_default').val($(this).text());
+	});
+
+	function print_dict_assignment()
+	{
+		$('pre').text(JSON.stringify(glob_dict_assignemnts, null, 2));
+	}
+});
--- mturk/mturk_manager/views/view.py
+++ mturk/mturk_manager/views/view.py
@@ -27,18 +27,22 @@ def view(request, name):
 
 
     if request.method == 'POST':
-        if request.POST['task'] == 'button_mturk_approve_selected':
-            list_ids = request.POST.getlist('checkbox_assignment')
-            approve_assignments(request, db_obj_project, list_ids)
-        if request.POST['task'] == 'button_mturk_reject_selected':
-            list_ids = request.POST.getlist('checkbox_assignment')
-            reject_assignments(request, db_obj_project, list_ids)
-        if request.POST['task'].startswith('button_mturk_approve__'):
-            id_assignment = request.POST['task'].split('__')[1]
-            approve_assignments(request, db_obj_project, [id_assignment])
-        if request.POST['task'].startswith('button_mturk_reject__'):
-            id_assignment = request.POST['task'].split('__')[1]
-            reject_assignments(request, db_obj_project, [id_assignment])
+        obj = json.loads(request.body.decode("utf-8"))
+
+        if obj['task'] == 'submit_annotations':
+            submit_annotations(request, db_obj_project, obj)
+        # if request.POST['task'] == 'button_mturk_approve_selected':
+        #     list_ids = request.POST.getlist('checkbox_assignment')
+        #     approve_assignments(request, db_obj_project, list_ids)
+        # if request.POST['task'] == 'button_mturk_reject_selected':
+        #     list_ids = request.POST.getlist('checkbox_assignment')
+        #     reject_assignments(request, db_obj_project, list_ids)
+        # if request.POST['task'].startswith('button_mturk_approve__'):
+        #     id_assignment = request.POST['task'].split('__')[1]
+        #     approve_assignments(request, db_obj_project, [id_assignment])
+        # if request.POST['task'].startswith('button_mturk_reject__'):
+        #     id_assignment = request.POST['task'].split('__')[1]
+        #     reject_assignments(request, db_obj_project, [id_assignment])
 
         return HttpResponseRedirect(reverse('mturk_manager:view', args=[name_quoted])+ '?list_ids='+request.GET['list_ids'])
 
@@ -68,6 +72,52 @@ def view(request, name):
     context['name_project'] = name_project
     context['queryset_messages_reject'] = m_Message_Reject.objects.filter(fk_project=db_obj_project)
     return render(request, 'mturk_manager/view.html', context)
+
+def submit_annotations(request, db_obj_project, obj):
+    client_sandbox = code_shared.get_client(db_obj_project, True)
+    client_real = code_shared.get_client(db_obj_project, False)
+    db_obj_tag_submitted = m_Tag.objects.get(key_corpus=db_obj_project.name, name='submitted')
+    db_obj_tag_approved = m_Tag.objects.get(key_corpus=db_obj_project.name, name='approved')
+    db_obj_tag_rejected = m_Tag.objects.get(key_corpus=db_obj_project.name, name='rejected')
+
+    dict_annotations = obj['dict_assignments']
+    message_reject_default = obj['message_reject_default']
+    list_ids = [id_assignment for id_assignment in dict_annotations.keys()]
+    for assignment in m_Assignment.objects.filter(id__in=list_ids):
+        dict_annotation = dict_annotations[str(assignment.id)]
+
+        feedback = ''
+        try:
+            feedback = dict_annotation['message']
+        except KeyError:
+            feedback = message_reject_default
+
+        print(feedback)
+        print(assignment.fk_hit.fk_batch.use_sandbox)
+        client = client_sandbox if assignment.fk_hit.fk_batch.use_sandbox else client_real
+        try:
+            pass
+            # if dict_annotation['state'] == 'approve':
+            #     response = client.approve_assignment(
+            #         AssignmentId=assignment.id_assignment,
+            #         RequesterFeedback=feedback
+            #     )
+            # elif dict_annotation['state'] == 'reject':
+            #     response = client.reject_assignment(
+            #         AssignmentId=assignment.id_assignment,
+            #         RequesterFeedback=feedback
+            #     )
+        except Exception as e:
+            continue
+
+        if dict_annotation['state'] == 'approve':
+            db_obj_tag_submitted.items.remove(assignment)
+            db_obj_tag_approved.items.add(assignment)
+        elif dict_annotation['state'] == 'reject':
+            db_obj_tag_submitted.items.remove(assignment)
+            db_obj_tag_rejected.items.add(assignment)
+
+    print(obj)
 
 def approve_assignments(request, db_obj_project, list_ids):
     client_sandbox = code_shared.get_client(db_obj_project, True)
