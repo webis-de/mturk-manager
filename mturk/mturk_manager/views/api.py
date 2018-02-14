ba66e875975efae4c8af0771a75f6d74fd951313
Kristof Komlossy <yiro4618@webis24.medien.uni-weimar.de>
2018-02-14 15:21:13 +0100

    *continued un-/blocking of workers

--- mturk/mturk_manager/views/api.py
+++ mturk/mturk_manager/views/api.py
@@ -1,3 +1,35 @@
+import urllib.parse
+from django.http import JsonResponse
+from mturk_manager.models import *
+import json
+
 def api(request, name):
-	print(name)
-	return
+    name_quoted = name
+    name_project = urllib.parse.unquote(name_quoted)
+    db_obj_project = m_Project.objects.get(
+        name=name_project
+    )
+
+    if request.method == 'POST':
+        if request.POST['task'] == 'unblock_workers':
+            unblock_workers(request, db_obj_project)
+        elif request.POST['task'] == 'block_workers':
+            block_workers(request, db_obj_project)
+
+    return JsonResponse({})
+
+def unblock_workers(request, db_obj_project):
+    list_ids = request.POST.getlist('list_ids[]')
+
+    m_Worker.objects.filter(
+        fk_project=db_obj_project,
+        id__in=list_ids
+    ).update(is_blocked=False)
+
+def block_workers(request, db_obj_project):
+    list_ids = request.POST.getlist('list_ids[]')
+
+    m_Worker.objects.filter(
+        fk_project=db_obj_project,
+        id__in=list_ids
+    ).update(is_blocked=True)
