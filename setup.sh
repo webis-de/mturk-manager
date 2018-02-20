c481362046a8247e14a83b654f89cae69859763f
Kristof Komlossy <yiro4618@webis24.medien.uni-weimar.de>
2018-02-20 10:30:05 +0100

    *requirements

--- setup.sh
+++ setup.sh
@@ -4,10 +4,8 @@
 git pull
 git submodule update --recursive --remote --init
 
-pip3 install -r corpus-viewer/requirements.txt
 pip3 install -r requirements.txt
 
 cd ./mturk
 python3 manage.py createcachetable
-# python3 manage.py makemigrations viewer mturk_manager
 python3 manage.py migrate
