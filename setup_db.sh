0f6de95beb400405b009841b77aa675a384085a0
Kristof Komlossy <contact@kritten.org>
2018-11-10 18:59:23 +0100

    *improved setup_db.sh

--- setup_db.sh
+++ setup_db.sh
@@ -2,9 +2,14 @@
 # do necessary inital tasks
 
 git pull
-git submodule update --recursive --remote --init
+# git submodule update --recursive --remote --init
 
-pip3 install -r requirements.txt
+pip3 install boto3
+pip3 install xmltodict
+pip3 install djangorestframework
+pip3 install django-cors-headers
+
+# pip3 install -r requirements.txt
 
 cd ./mturk_db
 python3 manage.py migrate
