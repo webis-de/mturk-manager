06f7667f07b5a19fa0ed8c53362f583e0c71ed9a
Kristof Komlossy <contact@kritten.org>
2018-11-16 23:43:27 +0100

    *moved parcel scripts to package.json

--- mturk/parcel.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-./node_modules/.bin/parcel ./mturk_manager/static/mturk_manager/js/vue/index.js -d ./mturk_manager/static/mturk_manager/js/dist
-# ./node_modules/.bin/parcel watch ./mturk_manager/static/mturk_manager/js/vue/index.js -d ./mturk_manager/static/mturk_manager/js/dist
--- mturk/parcel_annotate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# ./node_modules/.bin/parcel build ./mturk_manager/static/mturk_manager/js/index_annotate.js -d ./mturk_manager/static/mturk_manager/js/dist
-./node_modules/.bin/parcel build ./mturk_manager/static/mturk_manager/js/annotate/index_annotate.js -d ./mturk_manager/static/mturk_manager/js/dist
-# ./node_modules/.bin/parcel watch ./mturk_manager/static/mturk_manager/js/annotate/index_annotate.js -d ./mturk_manager/static/mturk_manager/js/dist
