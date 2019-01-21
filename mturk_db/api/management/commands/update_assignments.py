fd1057a9ab9ebe18ad92494a43e945495c44e6c3
Kristof Komlossy <contact@kritten.org>
2019-01-21 21:51:20 +0100

    *improved update_assignments command

--- mturk_db/api/management/commands/update_assignments.py
+++ mturk_db/api/management/commands/update_assignments.py
@@ -3,6 +3,7 @@ from django.db.models import Q
 
 from api.classes import Manager_Projects
 from api.models import Account_Mturk, Assignment
+from api.enums import assignments
 
 
 class Command(BaseCommand):
@@ -24,10 +25,19 @@ class Command(BaseCommand):
 
         count = 0
         for index, assignment_db in enumerate(Assignment.objects.filter(hit__batch__use_sandbox=use_sandbox).filter(Q(datetime_submit=None) | Q(datetime_accept=None))):
-            assignment_mturk = mturk_api.get_assignment(AssignmentId=assignment_db.id_assignment)['Assignment']
-
+            try:
+                assignment_mturk = mturk_api.get_assignment(AssignmentId=assignment_db.id_assignment)['Assignment']
+            except:
+                continue
             assignment_db.datetime_accept = assignment_mturk['AcceptTime']
             assignment_db.datetime_submit = assignment_mturk['SubmitTime']
+
+            status = assignment_mturk['AssignmentStatus']
+            if status == 'Approved':
+                assignment_db.status_external = assignments.STATUS_EXTERNAL.APPROVED
+            elif status == 'Rejected':
+                assignment_db.status_external = assignments.STATUS_EXTERNAL.REJECTED
+
             assignment_db.save()
             count += 1
             if index % 100 == 0:
