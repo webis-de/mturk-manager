cbb8c2b19894e2784a2aea07dbc08af43b4996b7
KKomlossy <kristof.komlossy@bpc.ag>
2019-02-03 19:51:27 +0100

    +update notification

---
+++ mturk/mturk_manager/static/mturk_manager/js/vue/components/help-icon.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-menu
+        offset-y
+        offset-x
+        v-bind:close-on-content-click="false"
+    >
+        <v-btn
+            icon
+            small
+            slot="activator"
+        >
+            <v-icon>help</v-icon>
+        </v-btn>
+        <v-card>
+            <v-card-text>
+                <slot></slot>
+            </v-card-text>
+        </v-card>
+    </v-menu>
+</template>
+
+<script>
+    export default {
+        name: "help-icon"
+    }
+</script>
+
+<style scoped>
+    .v-btn {
+        padding: 0;
+    }
+</style>
--- mturk/mturk_manager/static/mturk_manager/js/vue/components/the-update-notification.vue
+++ mturk/mturk_manager/static/mturk_manager/js/vue/components/the-update-notification.vue
@@ -4,16 +4,21 @@
         v-bind:value="version_api > version_app"
         v-bind:timeout="0"
         color="info"
+        auto-height
         >
-        <v-spacer></v-spacer>Update Available!<v-spacer></v-spacer>
+        <v-spacer></v-spacer>
+            Update Available! <help-icon>Pull the latest changes of the git repository to update the application.</help-icon>
+        <v-spacer></v-spacer>
     </v-snackbar>
 </template>
 
 <script>
     import {mapState} from "vuex";
+    import HelpIcon from "./help-icon";
 
     export default {
         name: "the-update-notification",
+        components: {HelpIcon},
         data() {
             return {}
         },
