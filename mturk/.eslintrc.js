9d2dccfecddbf95a888bd831ce3b01874782dd91
KKomlossy <kristof.komlossy@bpc.ag>
2019-02-14 18:29:48 +0100

    *linted config files

--- mturk/.eslintrc.js
+++ mturk/.eslintrc.js
@@ -2,18 +2,18 @@ module.exports = {
   root: true,
 
   env: {
-    node: true
+    node: true,
   },
 
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
 
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
 
   // extends: ["plugin:vue/essential", "@vue/prettier", "@vue/airbnb"]
-  extends: ["plugin:vue/essential", "@vue/airbnb"]
+  extends: ['plugin:vue/essential', '@vue/airbnb'],
 };
--- mturk/vue.config.js
+++ mturk/vue.config.js
@@ -1,29 +1,29 @@
 const webpack = require('webpack');
-const CircularDependencyPlugin = require('circular-dependency-plugin')
-const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+const CircularDependencyPlugin = require('circular-dependency-plugin');
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin');
 
 module.exports = {
-    runtimeCompiler: true,
-    publicPath: '/static/mturk_manager/js/dist',
-    outputDir: 'mturk_manager/static/mturk_manager/js/dist',
-    indexPath: '../../../../../mturk_manager/templates/mturk_manager/app.html',
-    configureWebpack: {
-        plugins: [
-            new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /en-gb/),
-            new CircularDependencyPlugin({
-                // exclude detection of files based on a RegExp
-                exclude: /a\.js|node_modules/,
-                // add errors to webpack instead of warnings
-                failOnError: true,
-                // set the current working directory for displaying module paths
-                cwd: process.cwd(),
-            }),
-            new VuetifyLoaderPlugin()
-        ]
+  runtimeCompiler: true,
+  publicPath: '/static/mturk_manager/js/dist',
+  outputDir: 'mturk_manager/static/mturk_manager/js/dist',
+  indexPath: '../../../../../mturk_manager/templates/mturk_manager/app.html',
+  configureWebpack: {
+    plugins: [
+      new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /en-gb/),
+      new CircularDependencyPlugin({
+        // exclude detection of files based on a RegExp
+        exclude: /a\.js|node_modules/,
+        // add errors to webpack instead of warnings
+        failOnError: true,
+        // set the current working directory for displaying module paths
+        cwd: process.cwd(),
+      }),
+      new VuetifyLoaderPlugin(),
+    ],
+  },
+  pluginOptions: {
+    webpackBundleAnalyzer: {
+      openAnalyzer: false,
     },
-    pluginOptions: {
-        webpackBundleAnalyzer: {
-            openAnalyzer: false
-        }
-    }
+  },
 };
