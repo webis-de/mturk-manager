939d56f8987fb2b867b4c574cfa2610a04dd98c6
Kristof Komlossy <contact@kritten.org>
2019-02-15 19:02:34 +0100

    *linted config files

--- mturk/.eslintrc.js
+++ mturk/.eslintrc.js
@@ -3,20 +3,20 @@ module.exports = {
 
   env: {
     node: true,
-    browser: true
+    browser: true,
   },
 
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
 
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint',
   },
 
   extends: [
-    "plugin:vue/essential",
-    "@vue/airbnb",
-  ]
+    'plugin:vue/essential',
+    '@vue/airbnb',
+  ],
 };
