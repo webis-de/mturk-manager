bb0caa71ceb06691663484558811c545a9578512
KKomlossy <kristof.komlossy@bpc.ag>
2019-03-21 16:23:35 +0100

    *fixed #21 Improve 'Connection Error' page

--- mturk/mturk_manager/static/mturk_manager/js/vue/components/connection_error/app_connection_error.vue
+++ mturk/mturk_manager/static/mturk_manager/js/vue/components/connection_error/app_connection_error.vue
@@ -1,19 +1,23 @@
 <template>
   <v-container>
-    <v-layout wrap>
-      <v-flex class="text-xs-center">
-        <h3 class="display-1">
-          Connection Error
-        </h3>
-        <p>
-          No connection to the database could be established!
-        </p>
-        <v-btn
-          color="primary"
-          to="add_credentials"
-        >
-          Update credentials
-        </v-btn>
+    <v-layout fill-height column justify-center>
+      <v-flex shrink>
+        <v-layout justify-center>
+          <v-flex class="text-xs-center">
+            <h3 class="display-1">
+              Connection Error
+            </h3>
+            <p>
+              No connection to the database could be established!
+            </p>
+            <v-btn
+              color="primary"
+              to="add_credentials"
+            >
+              Update credentials
+            </v-btn>
+          </v-flex>
+        </v-layout>
       </v-flex>
     </v-layout>
   </v-container>
