1a5830681ef1f9453f1c151940ddb047aa911610
Kristof Komlossy <contact@kritten.org>
2019-04-21 18:11:15 +0200

    *improved override_rejection command

--- mturk_db/api/management/commands/override_rejection.py
+++ mturk_db/api/management/commands/override_rejection.py
@@ -6,12 +6,21 @@ from api.classes import Manager_Projects
 class Command(BaseCommand):
     help = 'Override Rejection'
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'assignments',
+            nargs='+',
+            help='List of assignments',
+            type=str
+        )
+
     def handle(self, *args, **options):
         client = Manager_Projects.get_mturk_api(False)
 
-        result = client.approve_assignment(
-            AssignmentId='',
-            OverrideRejection=True
-        )
+        for assignment in options['assignments']:
+            result = client.approve_assignment(
+                AssignmentId=assignment,
+                OverrideRejection=True
+            )
 
-        self.stdout.write(self.style.SUCCESS(result))
+            self.stdout.write(self.style.SUCCESS(result))
