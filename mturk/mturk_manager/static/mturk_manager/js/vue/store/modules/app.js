7ffed152fb7cd0aa466f01cbcea4760753f19afe
Kristof Komlossy <contact@kritten.org>
2019-06-21 17:55:16 +0200

    release 1.5.0

--- mturk/mturk_manager/static/mturk_manager/js/vue/store/modules/app.js
+++ mturk/mturk_manager/static/mturk_manager/js/vue/store/modules/app.js
@@ -10,7 +10,7 @@ export const module_app = _.merge({}, baseModule, {
     token_instance: null,
     use_sandbox: true,
     version_api: null,
-    version: '1.4.0',
+    version: '1.5.0',
     changelog: [],
     versionSeen: null,
   },
