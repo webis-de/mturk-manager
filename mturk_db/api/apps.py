ce4907539b4a7bd0dfed29bcd1cb7346ccb369c0
Kristof Komlossy <mturk@kritten.org>
2019-07-01 20:12:46 +0200

    *improved app startup

--- mturk_db/api/apps.py
+++ mturk_db/api/apps.py
@@ -1,28 +1,36 @@
 from django.apps import AppConfig
 from django.db.backends.signals import connection_created
 import logging
+from django.db import OperationalError
 
 def new_connection(**kwargs):
     from api.models import Account_Mturk
     from django.contrib.auth.models import User
 
-    if Account_Mturk.objects.count() == 0:
-        logger = logging.getLogger(__name__)
-        logger.critical('######################################################################################')
-        logger.critical('# You did not add an MTurk account. Execute \'python3 manage.py set_mturk_account -h\' #')
-        logger.critical('######################################################################################')
-
     try:
-        worker = User.objects.get(username='worker')
-        instance = User.objects.get(username='instance')
+        if Account_Mturk.objects.count() == 0:
+            logger = logging.getLogger(__name__)
+            logger.critical('######################################################################################')
+            logger.critical('# You did not add an MTurk account. Execute \'python3 manage.py set_mturk_account -h\' #')
+            logger.critical('######################################################################################')
 
-        worker.auth_token
-        instance.auth_token
-    except Exception:
+        try:
+            worker = User.objects.get(username='worker')
+            instance = User.objects.get(username='instance')
+
+            worker.auth_token
+            instance.auth_token
+        except Exception:
+            logger = logging.getLogger(__name__)
+            logger.critical('######################################################################################')
+            logger.critical('# You did not set the authentication tokens. Execute \'python3 manage.py set_tokens -h\' #')
+            logger.critical('######################################################################################')
+    except OperationalError:
         logger = logging.getLogger(__name__)
         logger.critical('######################################################################################')
-        logger.critical('# You did not set the authentication tokens. Execute \'python3 manage.py set_tokens -h\' #')
+        logger.critical('# Execute \'python3 manage.py migrate\' to initialize the database #')
         logger.critical('######################################################################################')
+
 
 class ApiConfig(AppConfig):
     name = 'api'
