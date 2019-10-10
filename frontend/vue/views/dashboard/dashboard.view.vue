b97bb4d0a708803704bb4f4d4cdf58ddb66ea7ea
Kristof Komlossy <mturk@kritten.org>
2019-10-10 20:14:46 +0200

    *improved dashboard

--- frontend/vue/views/dashboard/dashboard.view.vue
+++ frontend/vue/views/dashboard/dashboard.view.vue
@@ -1,18 +1,33 @@
 <template>
-  <v-content>
-    <v-container fluid>
-      <v-row no-gutters>
-        <v-col>
+  <div>
+    <v-app-bar
+      app
+      fixed
+      clipped-left
+      dense
+      v-bind:style="{
+        'background-color': $store.state.module_app.use_sandbox === true ? '#dd6e00' : null
+      }"
+    >
+      <v-toolbar-title style="position:absolute; left: 50%; margin-left: -70.76px">
+        MTurk-Manager
+      </v-toolbar-title>
+<!--      <v-toolbar-title>-->
+<!--        Dashboard-->
+<!--      </v-toolbar-title>-->
+
+      <v-row
+        align="center"
+        justify="end"
+        no-gutters
+      >
+        <v-col class="shrink">
           <v-menu
-            v-model="dialog"
             v-bind:close-on-content-click="false"
             offset-y
           >
             <template v-slot:activator="{ on }">
               <v-btn
-                absolute
-                top
-                right
                 icon
                 v-on="on"
               >
@@ -22,43 +37,50 @@
 
             <the-settings-app v-on:updated-credentials="dialog = false" />
           </v-menu>
-
-          <v-row no-gutters>
-            <v-col>
-              <component-create-project />
-            </v-col>
-          </v-row>
-
-          <v-divider class="my-3" />
-
-          <v-row no-gutters>
-            <v-col>
-              <open-project />
-            </v-col>
-          </v-row>
         </v-col>
       </v-row>
+    </v-app-bar>
+    <v-content>
+      <v-container fluid>
+        <v-row no-gutters>
+          <v-col>
+            <v-row no-gutters>
+              <v-col>
+                <component-create-project />
+              </v-col>
+            </v-row>
 
-      <v-divider class="my-3" />
+            <v-divider class="my-3" />
 
-      <v-row
-        no-gutters
-        justify="end"
-      >
-        <v-col
-          class="shrink"
+            <v-row no-gutters>
+              <v-col>
+                <open-project />
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+
+        <v-divider class="my-3" />
+
+        <v-row
+          no-gutters
+          justify="end"
         >
-          <v-btn
-            small
-            color="secondary"
-            v-bind:to="{ name: 'about' }"
+          <v-col
+            class="shrink"
           >
-            About
-          </v-btn>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-content>
+            <v-btn
+              small
+              color="secondary"
+              v-bind:to="{ name: 'about' }"
+            >
+              About
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </div>
 </template>
 
 <script>
