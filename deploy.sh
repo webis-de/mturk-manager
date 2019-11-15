9cc376b58dddc939a458fd418a557db6c2503892
Kristof Komlossy <mturk@kritten.org>
2019-11-15 19:55:43 +0100

    +deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# adjust version in .env
+# adjust version in scripts/.env
+
+start_time="$(date -u +%s)"
+
+cd "$(dirname "$0")"
+
+export $(cat .env)
+
+###############################################################
+# Build and push backend
+###############################################################
+docker-compose -f mturk_db/docker-compose.yml build backend
+
+echo ${DOCKER_PASSWORD} | docker login --username ${DOCKER_USERNAME} --password-stdin
+
+docker tag ${DOCKER_IMAGE_BACKEND}:${VERSION_MTURK_MANAGER} ${DOCKER_USERNAME}/${DOCKER_IMAGE_BACKEND}:${VERSION_MTURK_MANAGER}
+
+docker push ${DOCKER_USERNAME}/${DOCKER_IMAGE_BACKEND}:${VERSION_MTURK_MANAGER}
+
+###############################################################
+# Build and push frontend
+###############################################################
+cd frontend
+
+npm run build
+npm run parcel_annotate_build
+npm run parcel_sandbox_hit_build
+cp sandbox-hit/sandbox-hit.html dist
+cp annotate/view.html dist
+
+cd ..
+
+docker-compose -f frontend/docker-compose.yml build frontend
+
+docker tag ${DOCKER_IMAGE_FRONTEND}:${VERSION_MTURK_MANAGER} ${DOCKER_USERNAME}/${DOCKER_IMAGE_FRONTEND}:${VERSION_MTURK_MANAGER}
+
+docker push ${DOCKER_USERNAME}/${DOCKER_IMAGE_FRONTEND}:${VERSION_MTURK_MANAGER}
+
+end_time="$(date -u +%s)"
+elapsed="$(($end_time-$start_time))"
+echo "Finished in $elapsed seconds"
