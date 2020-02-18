22a7d4319caea6664a2e22ab549a0e00c1813cf7
Kristof Komlossy <mturk@kritten.org>
2020-02-18 19:22:24 +0100

    +base-page-group

---
+++ frontend/vue/components/base/base-page-group.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-row
+    v-bind:id="identifier.identifier !== undefined ? identifier.identifier : false"
+    dense
+  >
+    <v-col
+      v-for="basePageSection in $slots.default"
+      class="py-0"
+    >
+      <base-wrap-node
+        v-bind:value="basePageSection"
+      />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import BaseWrapNode from './base-wrap-node';
+
+export default {
+  name: 'BasePageGroup',
+  components: { BaseWrapNode },
+  props: {
+    identifier: {
+      type: Object,
+      required: false,
+      default() {
+        return {};
+      },
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
