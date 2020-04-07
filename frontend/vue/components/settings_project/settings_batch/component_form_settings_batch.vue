30a9ca61b0646381108ec4151e6f3747e0ad34a6
Kristof Komlossy <mturk@kritten.org>
2020-04-07 18:21:20 +0200

    *fixed chips

--- frontend/vue/components/settings_project/settings_batch/component_form_settings_batch.vue
+++ frontend/vue/components/settings_project/settings_batch/component_form_settings_batch.vue
@@ -1,60 +1,69 @@
 <template>
   <div>
     <h2>General Settings</h2>
-    <v-container fluid class="pa-0 pt-3" grid-list-md>
+    <v-container
+      fluid
+      class="pa-0 pt-3"
+      grid-list-md
+    >
       <v-layout wrap>
         <v-flex v-bind="flexSize">
           <v-text-field
             dense
             required
             v-bind:value="title"
-            v-on:input="
-              $emit('update:title', $event);
-              v.settings_batch.title.$touch();
-            "
             label="Title"
             v-bind:error-messages="validation_errors.settings_batch.title"
             v-bind:readonly="readonly"
             v-bind:disabled="disabled"
-          ></v-text-field>
+            v-on:input="
+              $emit('update:title', $event);
+              v.settings_batch.title.$touch();
+            "
+          />
         </v-flex>
         <v-flex v-bind="flexSize">
           <v-text-field
             dense
             required
             v-bind:value="description"
-            v-on:input="
-              $emit('update:description', $event);
-              v.settings_batch.description.$touch();
-            "
             label="Description"
             v-bind:error-messages="validation_errors.settings_batch.description"
             v-bind:readonly="readonly"
             v-bind:disabled="disabled"
-          ></v-text-field>
+            v-on:input="
+              $emit('update:description', $event);
+              v.settings_batch.description.$touch();
+            "
+          />
         </v-flex>
         <v-flex v-bind="flexSize">
-          <v-layout row align-center>
+          <v-layout
+            row
+            align-center
+          >
             <v-flex>
               <v-text-field
                 dense
                 required
                 type="number"
                 v-bind:value="reward"
-                v-on:input="
-                  $emit('update:reward', try_number($event));
-                  v.settings_batch.reward.$touch();
-                "
                 step="1"
                 v-bind:label="`Reward (${amount_formatted(reward)})`"
                 v-bind:error-messages="validation_errors.settings_batch.reward"
                 v-bind:readonly="readonly"
                 v-bind:disabled="disabled"
-              >
-              </v-text-field>
+                v-on:input="
+                  $emit('update:reward', try_number($event));
+                  v.settings_batch.reward.$touch();
+                "
+              />
             </v-flex>
             <v-flex shrink>
-              <div slot="append" class="text-no-wrap">
+              <div
+                slot="append"
+                class="text-no-wrap"
+              >
                 ct, in Dollar: {{ amount_formatted(reward) }}
               </div>
             </v-flex>
@@ -66,16 +75,16 @@
             required
             type="number"
             v-bind:value="count_assignments"
-            v-on:input="
-              $emit('update:count_assignments', try_number($event));
-              v.settings_batch.count_assignments.$touch();
-            "
             label="Number of Assignments"
             min="1"
             v-bind:error-messages="validation_errors.settings_batch.count_assignments"
             v-bind:readonly="readonly"
             v-bind:disabled="disabled"
-          ></v-text-field>
+            v-on:input="
+              $emit('update:count_assignments', try_number($event));
+              v.settings_batch.count_assignments.$touch();
+            "
+          />
 
           <!-- <v-text-field
                   required
@@ -95,17 +104,17 @@
             required
             type="number"
             v-bind:value="lifetime"
-            v-on:input="
-              $emit('update:lifetime', try_number($event));
-              v.settings_batch.lifetime.$touch();
-            "
             v-bind:label="format_duration('Lifetime', lifetime)"
             min="1"
             suffix="s"
             v-bind:error-messages="validation_errors.settings_batch.lifetime"
             v-bind:readonly="readonly"
             v-bind:disabled="disabled"
-          ></v-text-field>
+            v-on:input="
+              $emit('update:lifetime', try_number($event));
+              v.settings_batch.lifetime.$touch();
+            "
+          />
         </v-flex>
         <v-flex v-bind="flexSize">
           <v-text-field
@@ -113,26 +122,25 @@
             required
             type="number"
             v-bind:value="duration"
-            v-on:input="
-              $emit('update:duration', try_number($event));
-              v.settings_batch.duration.$touch();
-            "
             v-bind:label="format_duration('Duration', duration)"
             min="1"
             suffix="s"
             v-bind:error-messages="validation_errors.settings_batch.duration"
             v-bind:readonly="readonly"
             v-bind:disabled="disabled"
-          ></v-text-field>
+            v-on:input="
+              $emit('update:duration', try_number($event));
+              v.settings_batch.duration.$touch();
+            "
+          />
         </v-flex>
-        <v-flex v-if="disabled === false" v-bind="flexSize">
+        <v-flex
+          v-if="disabled === false"
+          v-bind="flexSize"
+        >
           <v-select
             dense
             v-bind:value="template"
-            v-on:input="
-              $emit('update:template', $event);
-              v.settings_batch.template.$touch();
-            "
             v-bind:items="arrayTemplatesWorker"
             label="Worker Template"
             item-text="name"
@@ -140,7 +148,11 @@
             v-bind:error-messages="validation_errors.settings_batch.template"
             v-bind:readonly="readonly"
             v-bind:disabled="disabled"
-          ></v-select>
+            v-on:input="
+              $emit('update:template', $event);
+              v.settings_batch.template.$touch();
+            "
+          />
         </v-flex>
         <v-flex v-bind="flexSize">
           <v-switch
@@ -150,23 +162,19 @@
               })`
             "
             v-bind:input-value="block_workers"
+            v-bind:error-messages="validation_errors.settings_batch.block_workers"
+            v-bind:readonly="readonly"
+            v-bind:disabled="disabled"
             v-on:change="
               $emit('update:block_workers', $event);
               v.settings_batch.block_workers.$touch();
             "
-            v-bind:error-messages="validation_errors.settings_batch.block_workers"
-            v-bind:readonly="readonly"
-            v-bind:disabled="disabled"
-          ></v-switch>
+          />
         </v-flex>
         <v-flex v-bind="flexSize">
           <v-combobox
             dense
             v-bind:value="keywords"
-            v-on:input="
-              $emit('update:keywords', $event);
-              v.settings_batch.keywords.$touch();
-            "
             label="Keywords (Separated with TAB)"
             v-bind:items="list_keywords"
             hide-selected
@@ -176,43 +184,53 @@
             counter
             append-icon
             v-bind:error-messages="validation_errors.settings_batch.keywords"
-            v-on:change="handle_change_combobox($event)"
             v-bind:readonly="readonly"
             v-bind:disabled="disabled"
+            v-on:input="
+              $emit('update:keywords', $event);
+              v.settings_batch.keywords.$touch();
+            "
+            v-on:change="handle_change_combobox($event)"
           >
-            <template slot="selection" slot-scope="data">
+            <template
+              slot="selection"
+              slot-scope="data"
+            >
               <v-chip
-                v-bind:selected="data.selected"
+                v-bind:input-value="data.selected"
                 close
                 small
-                @input="remove(data.item)"
+                v-on:input="remove(data.item)"
               >
                 <strong>{{
                   data.item.text != undefined ? data.item.text : data.item
-                }}</strong
-                >&nbsp;
+                }}</strong>&nbsp;
               </v-chip>
             </template>
           </v-combobox>
         </v-flex>
       </v-layout>
     </v-container>
-    <v-divider class="my-3"></v-divider>
+    <v-divider class="my-3" />
     <h2>Qualifications</h2>
-    <v-container fluid class="pa-0" grid-list-md>
+    <v-container
+      fluid
+      class="pa-0"
+      grid-list-md
+    >
       <v-layout wrap>
         <v-flex v-bind="flexSize">
           <v-switch
             v-bind:label="`Contains Adult Content`"
             v-bind:input-value="has_content_adult"
+            v-bind:error-messages="validation_errors.settings_batch.has_content_adult"
+            v-bind:readonly="readonly"
+            v-bind:disabled="disabled"
             v-on:change="
               $emit('update:has_content_adult', $event);
               v.settings_batch.has_content_adult.$touch();
             "
-            v-bind:error-messages="validation_errors.settings_batch.has_content_adult"
-            v-bind:readonly="readonly"
-            v-bind:disabled="disabled"
-          ></v-switch>
+          />
         </v-flex>
         <v-flex v-bind="flexSize">
           <v-text-field
@@ -220,10 +238,6 @@
             required
             type="number"
             v-bind:value="qualification_assignments_approved"
-            v-on:input="
-              $emit('update:qualification_assignments_approved', try_number($event));
-              v.settings_batch.qualification_assignments_approved.$touch();
-            "
             step="1"
             max="100"
             min="0"
@@ -234,7 +248,11 @@
             "
             v-bind:readonly="readonly"
             v-bind:disabled="disabled"
-          ></v-text-field>
+            v-on:input="
+              $emit('update:qualification_assignments_approved', try_number($event));
+              v.settings_batch.qualification_assignments_approved.$touch();
+            "
+          />
         </v-flex>
         <v-flex v-bind="flexSize">
           <v-text-field
@@ -242,10 +260,6 @@
             required
             type="number"
             v-bind:value="qualification_hits_approved"
-            v-on:input="
-              $emit('update:qualification_hits_approved', try_number($event));
-              v.settings_batch.qualification_hits_approved.$touch();
-            "
             step="1"
             min="0"
             label="Approved HITs"
@@ -254,16 +268,16 @@
             "
             v-bind:readonly="readonly"
             v-bind:disabled="disabled"
-          ></v-text-field>
+            v-on:input="
+              $emit('update:qualification_hits_approved', try_number($event));
+              v.settings_batch.qualification_hits_approved.$touch();
+            "
+          />
         </v-flex>
         <v-flex v-bind="flexSize">
           <v-combobox
             dense
             v-bind:value="qualification_locale"
-            v-on:input="
-              $emit('update:qualification_locale', $event);
-              v.settings_batch.qualification_locale.$touch();
-            "
             label="Locale (Separated with TAB)"
             hide-selected
             small-chips
@@ -274,21 +288,27 @@
             v-bind:error-messages="
               validation_errors.settings_batch.qualification_locale
             "
-            v-on:change="handle_change_combobox($event)"
             v-bind:readonly="readonly"
             v-bind:disabled="disabled"
+            v-on:input="
+              $emit('update:qualification_locale', $event);
+              v.settings_batch.qualification_locale.$touch();
+            "
+            v-on:change="handle_change_combobox($event)"
           >
-            <template slot="selection" slot-scope="data">
+            <template
+              slot="selection"
+              slot-scope="data"
+            >
               <v-chip
-                v-bind:selected="data.selected"
+                v-bind:input-value="data.selected"
                 close
                 small
-                @input="remove_qualification_locale(data.item)"
+                v-on:input="remove_qualification_locale(data.item)"
               >
                 <strong>{{
                   data.item.text != undefined ? data.item.text : data.item
-                }}</strong
-                >&nbsp;
+                }}</strong>&nbsp;
               </v-chip>
             </template>
           </v-combobox>
@@ -300,12 +320,11 @@
 
 <script>
 import {
-  mapState, mapMutations, mapActions, mapGetters,
+  mapState, mapGetters,
 } from 'vuex';
 import humanizeDuration from 'humanize-duration';
 import _ from 'lodash';
 import helpers from '../../../mixins/helpers.mixin';
-import {Service_Templates} from '../../../services/service_templates';
 
 export default {
   name: 'ComponentFormSettingsBatch',
@@ -333,7 +352,7 @@ export default {
         };
       },
     },
-    validation_errors: {
+    validationErrors: {
       required: false,
       default() {
         return {
@@ -344,17 +363,17 @@ export default {
     title: {},
     description: {},
     reward: {},
-    count_assignments: {},
-    count_assignments_max_per_worker: {},
+    countAssignments: {},
+    countAssignmentsMaxPerWorker: {},
     lifetime: {},
     duration: {},
     template: {},
-    block_workers: {},
+    blockWorkers: {},
     keywords: {},
-    has_content_adult: {},
-    qualification_assignments_approved: {},
-    qualification_hits_approved: {},
-    qualification_locale: {},
+    hasContentAdult: {},
+    qualificationAssignmentsApproved: {},
+    qualificationHitsApproved: {},
+    qualificationLocale: {},
   },
   data() {
     return {
